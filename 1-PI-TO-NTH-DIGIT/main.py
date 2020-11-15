d77ffd8bbd48f02fead6603a6af94a1e93223e78
anunay sinha <anusu90@gmail.com>
2020-11-15 19:52:04 +0530

    maximum digit limit fixed

--- 1-PI-TO-NTH-DIGIT/main.py
+++ 1-PI-TO-NTH-DIGIT/main.py
@@ -1,19 +1,26 @@
-#First we import math module
-
+#First we import math module and declare pi
 import math
-
-#Then we need to take input from the user
-n = int(input('Enter the number of digits after decimal that needs to be printed: '))
-
 pi = math.pi
 
-# Now two posibilities emerege:
-# 1. we round of:
-# 2. we dont round of:
+def piDigits (n):
+    # Now two posibilities emerege:
+    # 1. we round of:
+    # 2. we dont round of:
+    piDigitRounded = round(pi,n)
+    piDigitNotRounded = float(str(pi)[:n+2])
 
-piDigitRounded = round(pi,n)
-piDigitNotRounded = float(str(pi)[:n+2])
+    #printing the output
+    return (f'the value of pi upto {n} digits when rounded off is {piDigitRounded} and when not rounded off is {piDigitNotRounded}')
 
-#printing the output
+#WE WILL KEEP THE MAXIMUM DIGITS TO BE AT 20 AND MAXIMUM TRY TO BE 3
+attempt = 0
+while (attempt<3):
+    n = int(input('Enter the number of digits after decimal that needs to be printed: '))
+    if (n>20):
+        print("Maximum number of allowed digits is 20. Please try again")
+        attempt += 1
+    else:
+        print(piDigits(n))
+        break
 
-print(f'the value of pi upto {n} digits when rounded off is {piDigitRounded} and when not rounded off is {piDigitNotRounded}')
+if (attempt == 3): print('You have exceeded maximum limit. Try later.')
